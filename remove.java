bcc6d9aef2d8f7327e903ad4b5e54c8bb4844c7c
Kaustubh Rai <98734149+kaustubh0955@users.noreply.github.com>
2023-08-12 00:03:53 +0530

    Add files via upload

---
+++ remove.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class remove {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == val){
+                continue;
+            }
+            nums[count] = nums[i];
+            count++;
+        }
+        return count;
+    }
+}
